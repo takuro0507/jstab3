ee9b0da70ddad36734b7901c871b2bb1e2f07115
takuro0507 <82748532+takuro0507@users.noreply.github.com>
2021-04-18 15:59:29 +0900

    initial commit3
    練習です

---
+++ index.js
@@ -0,0 +1,48 @@
+(() => {
+  // ここに命令を書く
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS ='is-active';
+  const navLen = $nav.length;
+
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
+  // クリックしたら怒るイベント
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    // クリックされた後にnavとdataを取得
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
+
+    // 対象外のnav,content全て一旦リセットする
+    let index = 0;
+    while (index < navLen) {
+      $content[index].style.display = 'none';
+      $nav[index].classList.remove(ACTIVE_CLASS);
+      index++
+    }
+
+    // 対象のコンテンツをアクティブ化する
+    $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
+    console.log('$nav[targetVal].classList', $nav[targetVal].classList);
+
+  };
+
+  // 全nav要素に対して関数を適用
+  let index = 0;
+  while (index < navLen) {
+    $nav[index].addEventListener('click', (e) => handleClick(e));
+    index++
+  }
+
+
+
+
+})
+();
